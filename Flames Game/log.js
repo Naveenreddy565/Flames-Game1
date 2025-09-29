ce2b4de2536f43babed799b45e4b0e89cd404078
Naveenreddy565 <somulanaveenkumarreddy9@gmail.com>
2025-09-29 18:18:48 +0530

    Create log.js

---
+++ Flames Game/log.js
@@ -0,0 +1,10 @@
+// api/log.js
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    const { name1, name2 } = req.body;
+    console.log(`Name1: ${name1}, Name2: ${name2}`);
+    res.status(200).json({ message: "Names received!" });
+  } else {
+    res.status(405).json({ message: "Method not allowed" });
+  }
+}
